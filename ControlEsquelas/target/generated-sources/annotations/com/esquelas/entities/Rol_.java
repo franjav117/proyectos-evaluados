d6483dcd6f260df5155c3796efd7bf49970c8f94
Rogelio <Roger.Alonso7412@gmail.com>
2019-07-27 08:38:29 -0600

    Dao y Entity Esquela corregido

--- ControlEsquelas/target/generated-sources/annotations/com/esquelas/entities/Rol_.java
+++ ControlEsquelas/target/generated-sources/annotations/com/esquelas/entities/Rol_.java
@@ -6,15 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.7.0.v20170811-rNA", date="2019-07-26T08:23:18")
-=======
-@Generated(value="EclipseLink-2.7.0.v20170811-rNA", date="2019-07-26T08:17:05")
->>>>>>> develop
-=======
-@Generated(value="EclipseLink-2.7.0.v20170811-rNA", date="2019-07-26T10:20:43")
->>>>>>> feature_rogelio
+@Generated(value="EclipseLink-2.7.0.v20170811-rNA", date="2019-07-27T08:14:28")
 @StaticMetamodel(Rol.class)
 public class Rol_ { 
 
