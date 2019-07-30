e3188be4e7e1cf31d848c96c341f57fec859ae9f
Rogelio <Roger.Alonso7412@gmail.com>
2019-07-30 15:40:58 -0600

    Entities

--- ControlEsquelas/src/main/java/com/esquelas/entities/Esquela.java
+++ ControlEsquelas/src/main/java/com/esquelas/entities/Esquela.java
@@ -32,7 +32,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 @Table(name = "esquela")
 @XmlRootElement
 @NamedQueries({
-    @NamedQuery(name = "Esquela.findAll", query = "SELECT e FROM Esquela e")
+@NamedQuery(name = "Esquela.findAll", query = "SELECT e FROM Esquela e")
     , @NamedQuery(name = "Esquela.findByIdEsquela", query = "SELECT e FROM Esquela e WHERE e.idEsquela = :idEsquela")
     , @NamedQuery(name = "Esquela.findByConductor", query = "SELECT e FROM Esquela e WHERE e.idConductor.idConductor = :idConductor")
     , @NamedQuery(name = "Esquela.findByLicencia", query = "SELECT e FROM Esquela e WHERE e.idConductor.idConductor = :licencia")
@@ -40,6 +40,8 @@ import javax.xml.bind.annotation.XmlRootElement;
     , @NamedQuery(name = "Esquela.findByDUILicencia", query = "SELECT e FROM Esquela e WHERE e.idConductor.idPersona.dui = :dui and e.idConductor.licencia = :licencia")
     , @NamedQuery(name = "Esquela.findByPlaca", query = "SELECT e FROM Esquela e WHERE e.placa.idVehiculo = :placa")
     , @NamedQuery(name = "Esquela.findByCodigoFalta", query = "SELECT e FROM Esquela e WHERE e.codigoFalta = :codigoFalta")
+    , @NamedQuery(name = "Esquela.findByConductor", query = "SELECT e FROM Esquela e WHERE e.idConductor.idConductor = :idConductor")
+    , @NamedQuery(name = "Esquela.findByVehiculo", query = "SELECT e FROM Esquela e WHERE e.placa.idVehiculo = :idVehiculo")
     , @NamedQuery(name = "Esquela.findByFechaEsquela", query = "SELECT e FROM Esquela e WHERE e.fechaEsquela = :fechaEsquela")
     , @NamedQuery(name = "Esquela.findByLugarInfraccion", query = "SELECT e FROM Esquela e WHERE e.lugarInfraccion = :lugarInfraccion")
     , @NamedQuery(name = "Esquela.findByObservaciones", query = "SELECT e FROM Esquela e WHERE e.observaciones = :observaciones")
