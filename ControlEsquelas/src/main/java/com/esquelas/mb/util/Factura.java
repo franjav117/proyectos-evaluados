845516555e02f3bd02e87d0d12f0b677b074e4fd
Rogelio <Roger.Alonso7412@gmail.com>
2019-08-13 10:51:34 -0600

    Jasper, entidad espejo y controlador (aun vacio)

---
+++ ControlEsquelas/src/main/java/com/esquelas/mb/util/Factura.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.esquelas.mb.util;
+
+/**
+ *
+ * @author rogelio.mejiausam
+ */
+public class Factura {  //Controlador para manejar los Reportes
+    
+}
