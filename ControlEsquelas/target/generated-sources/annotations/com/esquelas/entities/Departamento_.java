37a69a149c0626a8fa084382233ac1803533476e
Francisco Amaya <francisco.2000.17.3@gmail.com>
2019-08-13 15:35:41 -0600

    13/08/2019
    detalles

--- ControlEsquelas/target/generated-sources/annotations/com/esquelas/entities/Departamento_.java
+++ ControlEsquelas/target/generated-sources/annotations/com/esquelas/entities/Departamento_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.0.v20170811-rNA", date="2019-08-10T09:10:25")
+@Generated(value="EclipseLink-2.7.0.v20170811-rNA", date="2019-08-13T15:32:21")
 @StaticMetamodel(Departamento.class)
 public class Departamento_ { 
 
